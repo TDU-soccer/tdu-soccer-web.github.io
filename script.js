6d94a72fe9a47316d740d7cac407acd1a2723cb9
obi-tdu <kimuobi28@icloud.com>
2024-11-09 18:06:30 +0800

    Update index.html, style.css, add script.js and new folder

---
+++ script.js
@@ -0,0 +1,20 @@
+// タブの切り替え
+function showCategory(category) {
+  var tabs = document.querySelectorAll('.tab-link');
+  var contents = document.querySelectorAll('.news-content');
+  
+  tabs.forEach(function(tab) {
+      tab.classList.remove('active');
+  });
+  contents.forEach(function(content) {
+      content.classList.remove('active');
+  });
+  
+  document.getElementById(category).classList.add('active');
+  document.querySelector(`[onclick="showCategory('${category}')"]`).classList.add('active');
+}
+
+// 折りたたみ機能
+function toggleDetail(element) {
+  element.classList.toggle('open');
+}
